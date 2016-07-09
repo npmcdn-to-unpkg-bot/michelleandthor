0317fc96cd5f6f1bf312984503c86104edf48897
jessiewuwu <jwu004@gmail.com>
2016-07-09 10:53:00 -0700

    Add Comment model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'main/index'
   root 'main#index'
 
+  resources :comments
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
