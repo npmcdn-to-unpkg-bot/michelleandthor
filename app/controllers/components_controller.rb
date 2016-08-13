c22f7cd6e71b5d7fdbeab3805d33320023dc901f
jessiewuwu <jwu004@gmail.com>
2016-08-12 20:38:13 -0700

    Fix the nil to_prompt problem by converting the value into Boolean. Add padding to the Gif card

--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -1,22 +1,12 @@
 class ComponentsController < ApplicationController
 
   def create_gif
-
-    p "---"*100
-    p params[:to_prompt]
-    if params[:to_prompt] == 1
-      to_prompt_boolean = true
-    else
-      to_prompt_boolean = false
-    end
-    p to_prompt_boolean
-
-    Component.create(content: params[:selectedGifUrl], author: params[:author], to_prompt: to_prompt_boolean, prompt_id: Prompt.last.id, category: "gif")
+    Component.create(content: params[:selectedGifUrl], author: params[:author], to_prompt: ActiveRecord::Type::Boolean.new.type_cast_from_database(params[:to_prompt]), prompt_id: Prompt.last.id, category: "gif")
     redirect_to root_path(anchor: "all-comments")
   end
 
   def create_comment
-    @comment = Component.create(author: params[:component][:author], content: params[:component][:content], to_prompt: params[:to_prompt],prompt_id: Prompt.last.id,category: 'comment')
+    @comment = Component.create(author: params[:component][:author], content: params[:component][:content], to_prompt: ActiveRecord::Type::Boolean.new.type_cast_from_database(params[:component][:to_prompt]), prompt_id: Prompt.last.id,category: 'comment')
 
     redirect_to root_path(anchor: "all-comments")
   end
